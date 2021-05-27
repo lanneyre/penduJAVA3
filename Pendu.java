17d8af5495d2f2f1d63dd34ff63ff8945a5ec64b
enrici <enrici@martel.ccinca.fr>
2021-05-27 12:30:13 +0200

    V2.1
    suppression d'un message de debug

--- Pendu.java
+++ Pendu.java
@@ -4,7 +4,7 @@ public class Pendu {
     public static void main(String[] args) throws Exception {
         Mot m = new Mot();
         m.motRandom();
-        System.out.println(m.motATrouver);
+        //System.out.println(m.motATrouver);
         //m.displayMot(new ArrayList<Character>());
 
         Utilisateur u = new Utilisateur();
